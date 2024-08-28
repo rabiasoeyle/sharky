a526e78bd0bc5daf2501a646c1f1a1c8516c9ef4
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2024-08-28 10:25:54 +0200

    status

--- classes/status.class.js
+++ classes/status.class.js
@@ -1,23 +1,56 @@
 class Statusbar extends DrawableObject{
     x = 40;
     y = 10;
-    width= 140;
+    width= 100;
     height= 50;
-    percentage;
+
+    poisonImage =[
+        'img/Marcadores/orange/0_poison.png',
+        'img/Marcadores/orange/20_poison.png',
+        'img/Marcadores/orange/40_poison.png',
+        'img/Marcadores/orange/60_poison.png',
+        'img/Marcadores/orange/80_poison.png',
+        'img/Marcadores/orange/100_poison.png',
+    ];
+    heartImage =[
+        'img/Marcadores/orange/0_heart.png',
+        'img/Marcadores/orange/20_heart.png',
+        'img/Marcadores/orange/40_heart.png',
+        'img/Marcadores/orange/60_heart.png',
+        'img/Marcadores/orange/80_heart.png',
+        'img/Marcadores/orange/100_heart.png',
+    ];
+    coinsImage =[
+        'img/Marcadores/orange/0_coin.png',
+        'img/Marcadores/orange/20_coin.png',
+        'img/Marcadores/orange/40_coin.png',
+        'img/Marcadores/orange/60_coin.png',
+        'img/Marcadores/orange/80_coin.png',
+        'img/Marcadores/orange/100_coin.png',
+    ]
+
+
+    heartPercentage = 100;
+    coinPercentage = 0;
+    poisonPercentage = 0;
 
     constructor(){
-        super();  
+        super();
+        this.loadImages(this.poisonImage);
+        this.loadImages(this.heartImage);
+        this.loadImages(this.coinsImage);  
+        this.setPercentage();
     }
 
-    setPercentage(images, percentage){
-        this.percentage = percentage;
+    setPercentage(heartPercentage){
+        this.heartPercentage = heartPercentage;
         // this.coinPercentage = coinPercentage;
         // this.poisonPercentage = poisonPercentage;
-        let path = this.images[this.resolveImageIndex()];
+        let path = this.heartImage[this.resolveHeartImageIndex()];
         this.img = this.imageCache[path];
     }
 
-    resolveImageIndex(){
+    resolveHeartImageIndex(){
         if(this.percentage == 100){
             return 5;
         }else if (this.heartPercentage > 80){
